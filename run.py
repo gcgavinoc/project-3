675008179c733c3dad4caf4ac2a3ef310800cc5b
gcgavinoc <gcgavinoc@gmail.com>
2022-10-11 13:26:56 +0000

    Create function for creating and placing ships on the game board

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from random import randint
 
 #Board for holding ship locations
-hidden_board = [[" "] * 8 for x in range(8)]
+hidden_board = [[" "] * 8 for i in range(8)]
 # Board for displaying hits and misses
 guess_board = [[" "] * 8 for i in range(8)]
+#Convert letter inputs to numbers
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 def print_board(board):
     """
@@ -16,4 +18,12 @@ def print_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
-print_board(hidden_board)
+def create_ships(board):
+    """
+    Create and place 5 ships on the board
+    """
+    for ship in range(5):
+        ship_row, ship_column = randint(0,7), randint(0,7)
+        while board[ship_row][ship_column] == "X":
+            ship_row, ship_column = get_ship_location()
+        board[ship_row][ship_column] = "X"
