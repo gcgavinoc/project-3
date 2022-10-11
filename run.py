35d82e64049d38b7b75070eab4df86f711502243
gcgavinoc <gcgavinoc@gmail.com>
2022-10-11 11:40:12 +0000

    Create function for printing game board to the console

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+#Board for holding ship locations
+hidden_board = [[" "] * 8 for x in range(8)]
+# Board for displaying hits and misses
+guess_board = [[" "] * 8 for i in range(8)]
+
+def print_board(board):
+    """
+    Print the game board to the console
+    """
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+print_board(hidden_board)
